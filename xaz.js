fa0a2482cd906f97b99eaf6360c72b90d2895440
aa1168 <linx63321@gmail.com>
2025-05-25 18:03:13 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,6 +1,6 @@
  
 
-var xz = "/xiazai/Lets-VIP.zip";
+var xz = "https://pub-a8564b270ee64f5c92e3b2b03fae65a3.r2.dev/latsvpn-latester.zip";
 
 //可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip
 //可以把   https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip  这个改成文件下载：/xiazai/kuailian.zip
